32d5f31d722b46fc9d384569de10d4d4393621e6
Muhammad Shayaan Nofil <136152497+Shayaan-Nofil@users.noreply.github.com>
2023-06-14 16:43:38 +0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,114 @@
+#include "graph.h"
+#include <gtest/gtest.h>
+
+//TEST(Graph, addNodetest) {
+//    Graph g;
+//    //arr = ReadFromFile();//this fuction returns edges and cities in 2d int array. Cities are mapped to int values
+//    //declared arraycount and V global for ease
+//    string s1 = "Sialkot:";
+//    g.addNode("Sialkot");
+//    //g->printGraph();
+//
+//    ASSERT_EQ(0, s1.compare(g.printsingleLink("Sialkot")));
+//}
+
+//TEST(Graph, addEdgetest) {
+//    Graph* g;
+//    //arr = ReadFromFile();//this fuction returns edges and cities in 2d int array. Cities are mapped to int values
+//    //declared arraycount and V global for ease
+//    string s1 = "Karachi: -> Sukkur-> Peshawar";
+//    g = addEdge("Karachi", "Peshawar", 200);
+//
+//    ASSERT_EQ(0, s1.compare(g->printsingleLink("Karachi")));
+//}
+
+//TEST(Graph, deleteNodetest) {
+//    Graph g;
+//    g.ReadFromFile();//this fuction returns edges and cities in 2d int array. Cities are mapped to int values
+//    //declared arraycount and V global for ease
+//    string s1 = "Multan: -> Jhang-> DG Khan-> Bahwalpur";
+//    g.deleteNode("Lahore");
+//    //g->printGraph();
+//
+//    ASSERT_EQ(0, s1.compare(g->printsingleLink("Multan")));
+//}
+
+//TEST(Graph, deleteEdgetest) {
+//    Graph* g;
+//    arr = ReadFromFile();//this fuction returns edges and cities in 2d int array. Cities are mapped to int values
+//    //declared arraycount and V global for ease
+//    string s1 = "Quetta:";
+//    g = deleteEdge("Quetta", "Khuzdar");
+//
+//
+//    ASSERT_EQ(0, s1.compare(g->printsingleLink("Quetta")));
+//}
+
+//TEST(Graph, shorterstPathfromOnetoalltest) {
+//    Graph g;
+//    g.ReadFromFile();
+//    g.shortestPathfromOnetoall("Lahore");
+//
+//    //Print all shortest Path from Lahore to all cities
+//
+//    //Evaluator will verify your output from terminal
+//
+//    ASSERT_EQ(0, 0);
+//}
+//
+//TEST(Graph, secondshorterstPathfromOnetoalltest) {
+//    Graph g;
+//    g.ReadFromFile();
+//    g.secondshortestPathfromOnetoall("Lahore");
+//
+//    //Print all shortest Path from Lahore to all cities
+//
+//    //Evaluator will verify your output from terminal
+//
+//    ASSERT_EQ(0, 0);
+//}
+//
+//TEST(Graph, shorterstPathfromalltoonetest) {
+//    Graph g;
+//    g.ReadFromFile();
+//    g.shortestPathfromalltoone("Lahore");
+//
+//    //Print all shortest Path from  all cities to Lahore
+//
+//    //Evaluator will verify your output from terminal
+//
+//    ASSERT_EQ(0, 0);
+//}
+//
+//TEST(Graph, secondshorterstPathfromalltoonetest) {
+//    Graph g;
+//    g.ReadFromFile();
+//    g.secondshortestPathfromalltoone("Lahore");
+//
+//    //Print all shortest Path from  all cities to Lahore
+//
+//    //Evaluator will verify your output from terminal
+//
+//    ASSERT_EQ(0, 0);
+//}
+//
+//TEST(Graph, ShortestRoutefrompairtest) {
+//    Graph g;
+//    g.ReadFromFile();
+//    //declared arraycount and V global for ease
+//    string s1 = "Mansehra-> Murree-> Islamabad";
+//    string s2 = g.ShortestRoutefrompair("Mansehra", "Islamabad");
+//
+//    ASSERT_EQ(0, s1.compare(s2));
+//}
+//
+//TEST(Graph, secondShortestRoutefrompairtest) {
+//    Graph g;
+//    g.ReadFromFile();
+//    //declared arraycount and V global for ease
+//    string s1 = "Mansehra-> Hassan Abdal-> Islamabad";
+//    string s2 = g.secondShortestRoutefrompair("Mansehra", "Islamabad");
+//
+//    ASSERT_EQ(0, s1.compare(s2));
+//}
+
